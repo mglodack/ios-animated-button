0e89f7df07257b0e930d93989806e935d78c5609
Mathew Glodack <glodack@gmail.com>
2019-03-05 00:07:30 -0800

    Initial Commit

---
+++ AnimatedButton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AnimatedButton
+//
+//  Created by Mathew Glodack on 3/5/19.
+//  Copyright © 2019 glodack. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
